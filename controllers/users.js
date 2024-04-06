4befb505170bdb13016a039f201bfae8c7fe18a7
StefanHritcu <shritcu@proton.me>
2024-04-06 16:40:39 +0200

    add: getFilteredUsers

--- controllers/users.js
+++ controllers/users.js
@@ -15,9 +15,27 @@ export const getUserById = async (req, res) => {
 
 export const getAllUsers = async (req, res) => {
     try{
+        //User are all the user(collection) documents
         const users = await User.find()
         res.status(200).json(users)
     } catch (error) {
         res.status(404).json({message: error.message})
     }
 }
+
+
+export const getFilteredUsers = async (req, res) => {
+    try {
+        // Costruisci la query Mongoose per trovare gli utenti con un'età maggiore o uguale a 18 anni e che sono sposati
+        const users = await User.find()
+            .where('age').gte(18) // Utenti con almeno 18 anni
+            .where('married').equals(true); // Utenti che sono sposati
+
+        // Invia gli utenti trovati come risposta
+        res.status(200).json(users);
+    } catch (error) {
+        // Gestisci eventuali errori
+        res.status(400).json({ message: error.message });
+    }
+};
+
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { getAllUsers, getUserById } from "../controllers/users.js"
+import { getAllUsers, getFilteredUsers, getUserById } from "../controllers/users.js"
 
 const router = express.Router()
 
-router.get("/:id", getUserById)
 router.get("/", getAllUsers)
+router.get("/filtered-users", getFilteredUsers)
+router.get("/:id", getUserById)
 
 export default router
